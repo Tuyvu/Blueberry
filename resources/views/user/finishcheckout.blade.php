f3c136590a4b033946055de87bfd053d26d77df9
Tuyvu <130845326+Tuyvu@users.noreply.github.com>
2024-10-12 17:29:44 +0700

    Update finishcheckout.blade.php

--- resources/views/user/finishcheckout.blade.php
+++ resources/views/user/finishcheckout.blade.php
@@ -5,7 +5,7 @@
 <section class="section-checkout padding-tb-50">
     <div class="container">
         <h1>Đặt hàng thành công</h1>
-        <a href="{{route('user.shiping')}}" class="btn btn-primary">theo dõi đơn hàng</a>                
+        <a href="{{route('user.shiping')}}" class="bb-btn-2">theo dõi đơn hàng</a>                
     </div>
 </section>
 
