940354d847e33ad4fe0acb1761f866e51c73f527
Tuyvu <130845326+Tuyvu@users.noreply.github.com>
2024-10-13 16:05:55 +0700

    Update categoty-nav.blade.php

--- resources/views/components/categoty-nav.blade.php
+++ resources/views/components/categoty-nav.blade.php
@@ -1,9 +1,5 @@
-<ul class="mega-menu">
-    <li>
-        <ul class="sub-menu">
-            @foreach ($category as $item)
-                <li><a href="{{route('user.findcategory',$item->name)}}">{{$item->name}}</a></li>
-            @endforeach
-        </ul>
-    </li>
+<ul class="sub-menu">
+    @foreach ($category as $item)
+        <li><a href="{{route('user.findcategory',$item->name)}}">{{$item->name}}</a></li>
+    @endforeach
 </ul>
