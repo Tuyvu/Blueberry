598aeca881ed678146b7c68a6458532b4b8b5f1b
Tuyvu <130845326+Tuyvu@users.noreply.github.com>
2024-10-16 21:46:52 +0700

    Update an.blade.php

--- resources/views/user/an.blade.php
+++ resources/views/user/an.blade.php
@@ -12,20 +12,22 @@
 
 <!-- Introduction Section -->
 <div class="container py-4">
-    <h2 class="text-center mb-4">GIỚI THIỆU VỀ Bluberry &amp; Bluberry+</h2>
+    <h2 class="text-center mb-4">GIỚI THIỆU VỀ Bluberry</h2>
     
-    <h3 class="text-body"><strong>Hệ thống Bluberry &amp; Bluberry+</strong></h3>
-    <p>Hệ thống siêu thị và chuỗi cửa hàng <a href="/" class="text-decoration-underline">Bluberry</a> &amp;<a href="/" class="text-decoration-underline"> Bluberry+</a> là hai thương hiệu bán lẻ thuộc <strong>[Công ty]</strong>. Ra đời từ năm 2014 cho đến nay, hệ thống Bluberry &amp; Bluberry+ không ngừng phát triển vươn lên, ra mắt với hơn 131 siêu thị Bluberry và gần 3000 cửa hàng Bluberry+ phủ rộng khắp Việt Nam, mang đến cho người tiêu dùng sự lựa chọn đa dạng về chất lượng hàng hóa và dịch vụ, đáp ứng đầy đủ nhu cầu trải nghiệm mua sắm từ bình dân đến cao cấp của mọi khách hàng.</p>
+    <h3 class="text-body"><strong>Hệ thống Bluberry</strong></h3>
+    <p>Hệ thống siêu thị và chuỗi cửa hàng <a href="/" class="text-decoration-underline">Bluberry</a> là hai thương hiệu bán lẻ thuộc <strong>[Công ty]</strong>. Ra đời từ năm 2014 cho đến nay, hệ thống Bluberry
+         không ngừng phát triển vươn lên, ra mắt với hơn 131 siêu thị Bluberry và gần 3000 cửa hàng Bluberry phủ rộng khắp Việt Nam, mang đến cho người tiêu dùng sự lựa chọn đa dạng về chất lượng hàng hóa và 
+         dịch vụ, đáp ứng đầy đủ nhu cầu trải nghiệm mua sắm từ bình dân đến cao cấp của mọi khách hàng.</p>
     
-    <p>Mua sắm tại <a href="/" class="text-decoration-underline">Bluberry</a> &amp;<a href="/" class="text-decoration-underline"> Bluberry+</a>, khách hàng không chỉ trải nghiệm thoải mái không gian dịch vụ tiện ích mà còn nhận được nhiều giá trị vượt trội với các hình thức khuyến mãi, ưu đãi hấp dẫn.</p>
+    <p>Mua sắm tại <a href="/" class="text-decoration-underline">Bluberry</a>, khách hàng không chỉ trải nghiệm thoải mái không gian dịch vụ tiện ích mà còn nhận được nhiều giá trị vượt trội với các hình thức khuyến mãi, ưu đãi hấp dẫn.</p>
 
     <!-- Mission Section -->
     <h3 class="text-body"><strong>Sứ mệnh</strong></h3>
-    <p>Với sứ mệnh phát triển bền vững và tạo được cho khách hàng tâm lý<strong> “AN TÂM MUA SẮM MỖI NGÀY”</strong>, Bluberry &amp; Bluberry+ luôn nỗ lực đáp ứng đầy đủ yêu cầu mua sắm của khách hàng, cam kết chất lượng khi sử dụng sản phẩm, giao hàng nhanh chóng, nâng cao giá trị cuộc sống của người tiêu dùng trong xã hội hiện đại.</p>
+    <p>Với sứ mệnh phát triển bền vững và tạo được cho khách hàng tâm lý<strong> “AN TÂM MUA SẮM MỖI NGÀY”</strong>, Bluberry luôn nỗ lực đáp ứng đầy đủ yêu cầu mua sắm của khách hàng, cam kết chất lượng khi sử dụng sản phẩm, giao hàng nhanh chóng, nâng cao giá trị cuộc sống của người tiêu dùng trong xã hội hiện đại.</p>
 
     <!-- Vision Section -->
     <h3 class="text-body"><strong>Tầm nhìn</strong></h3>
-    <p>Mang lại sự lựa chọn hoàn hảo về mua sắm tiện ích,<a href="/" class="text-decoration-underline"> Bluberry</a> &amp;<a href="/" class="text-decoration-underline"> Bluberry+</a> hướng tới mục tiêu trở thành hệ thống bán lẻ hàng đầu tại Việt Nam, dẫn đầu về cung cấp sản phẩm tiêu dùng và chất lượng dịch vụ trong đời sống người Việt.</p>
+    <p>Mang lại sự lựa chọn hoàn hảo về mua sắm tiện ích,<a href="/" class="text-decoration-underline"> Bluberry</a> hướng tới mục tiêu trở thành hệ thống bán lẻ hàng đầu tại Việt Nam, dẫn đầu về cung cấp sản phẩm tiêu dùng và chất lượng dịch vụ trong đời sống người Việt.</p>
     
     <p><a href="/" class="text-decoration-underline">Bluberry</a> còn chú trọng xây dựng môi trường làm việc chuyên nghiệp, năng động, sáng tạo và nhân văn; tạo điều kiện và cơ hội phát triển cho tất cả nhân viên; tích cực đóng góp vào các hoạt động xã hội, hướng về cộng đồng với những thông điệp mang tính nhân văn.</p>
 
@@ -44,9 +46,12 @@
     </ul>
 
     <!-- Additional Information Section -->
-    <p>Khu vực đông dân cư, giao thông thuận lợi; có diện tích lớn, cung cấp nhiều ngành hàng đa dạng. Với hàng chục ngàn mặt hàng từ thực phẩm, hóa mỹ phẩm, đồ dùng gia đình, điện máy gia dụng cho tới thời trang, đồ chơi… Bluberry &amp; Bluberry+ đáp ứng trọn vẹn nhu cầu của khách hàng địa phương và du khách.</p>
+    <p>Khu vực đông dân cư, giao thông thuận lợi; có diện tích lớn, cung cấp nhiều ngành hàng đa dạng. Với hàng chục ngàn mặt hàng từ thực phẩm, hóa mỹ phẩm, 
+        đồ dùng gia đình, điện máy gia dụng cho tới thời trang, đồ chơi… Bluberry đáp ứng trọn vẹn nhu cầu của khách hàng địa phương và du khách.</p>
     
-    <p>Với tầm nhìn dài hạn và mong muốn phát triển bền vững, mang đến cho người tiêu dùng Việt những trải nghiệm mua sắm tiện lợi với nhiều sự lựa chọn về sản phẩm, an tâm về chất lượng, hệ thống <a href="/" class="text-decoration-underline">Bluberry</a> &amp;<a href="/" class="text-decoration-underline"> Bluberry+</a> đã phủ rộng khắp Việt Nam với hơn 131 siêu thị Bluberry và gần 3000 cửa hàng Bluberry+.</p>
+    <p>Với tầm nhìn dài hạn và mong muốn phát triển bền vững, mang đến cho người tiêu dùng Việt những trải nghiệm mua sắm tiện lợi với nhiều sự lựa chọn về 
+        sản phẩm, an tâm về chất lượng, hệ thống <a href="/" class="text-decoration-underline">Bluberry</a> đã phủ rộng khắp Việt Nam với hơn 131 siêu thị Bluberry và 
+        gần 3000 cửa hàng Bluberry</p>
 
     <!-- Closing Section -->
     <h3 class="text-center text-footer"><strong>XIN CHÂN THÀNH CẢM ƠN</strong></h3>
