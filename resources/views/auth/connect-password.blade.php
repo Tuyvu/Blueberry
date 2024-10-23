341f8750a90108ffb192be8907a919958ee153ad
Tuyvu <130845326+Tuyvu@users.noreply.github.com>
2024-10-23 20:51:25 +0700

    Create connect-password.blade.php

---
+++ resources/views/auth/connect-password.blade.php
@@ -0,0 +1,20 @@
+@extends('user.master')
+
+<section class="section-login padding-tb-50">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title bb-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                    <div class="section-detail">
+                        <h2 class="bb-title">Vui lòng xem <span>Email</span></h2>
+                    </div>
+                </div>
+                <div class="section-title bb-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+                    <div class="justify-content-center align-items-center">
+                        <a href="https://mail.google.com/" class="bb-btn-2">Xem</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
