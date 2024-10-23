77755bd7f7e0be541b0531d4d741946a734b8b08
Tuyvu <130845326+Tuyvu@users.noreply.github.com>
2024-10-23 20:42:46 +0700

    Update comfirm.blade.php

--- resources/views/admin/order/comfirm.blade.php
+++ resources/views/admin/order/comfirm.blade.php
@@ -41,9 +41,9 @@
                                                 <img src="{{asset('storage/images')}}/{{$item->product->image}}" alt="" width="150px">
                                             </td>
                                             <td>{{$item->product->name}}</td>
-                                            <td>{{$item->price}}</td>
+                                            <td>{{ number_format($item->price, 0, ',', '.') }} đ</td>
                                             <td>{{$item->total_money/$item->price}}</td>
-                                            <td>{{$item->total_money}}</td>
+                                            <td>{{ number_format($order->total_money, 0, ',', '.') }} đ</td>
                                         </tr>
                                         @empty
                                         <tr>
@@ -54,45 +54,6 @@
                                     </tbody>
                                 </table>
                                 
-                            </div>
-                        </div><!--card-body-->
-                    </div><!--end card-->
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="row align-items-center">
-                                <div class="col">                      
-                                    <h4 class="card-title">Bought - Awaiting Delivery</h4>                      
-                                </div><!--end col-->
-                                <div class="col-auto">                      
-                                    <a href="#" class="text-secondary"><i class="fas fa-download me-1"></i> Download Invoice</a>                     
-                                </div><!--end col-->
-                            </div>  <!--end row-->                                  
-                        </div><!--end card-header-->
-                        <div class="card-body pt-0">
-                            <div class="position-relative m-4">
-                                <div class="progress" role="progressbar" aria-label="Progress" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="height: 1px;">
-                                  <div class="progress-bar" style="width: 50%"></div>
-                                </div>
-                                <div class="position-absolute top-0 start-0 translate-middle bg-primary text-white rounded-pill thumb-md"><i class="iconoir-home"></i></div>
-                                <div class="position-absolute top-0 start-50 translate-middle bg-primary-subtle text-primary rounded-pill thumb-md"><i class="iconoir-delivery-truck"></i></div>
-                                <div class="position-absolute top-0 start-100 translate-middle bg-light text-dark rounded-pill thumb-md"><i class="iconoir-map-pin"></i></div>
-                            </div>
-                            <div class="row row-cols-3">
-                                <div class="col text-start">
-                                    <h6 class="mb-1">Order Created</h6>
-                                    <p class="mb-0 text-muted fs-12 fw-medium">15 Feb 2024, 11:30 AM</p>
-                                </div> <!-- end col -->
-                                <div class="col text-center">
-                                    <h6 class="mb-1">On Delivery</h6>
-                                    <p class="mb-0 text-muted fs-12 fw-medium">18 Feb 2024, 05:15 PM</p>
-                                </div> <!-- end col -->
-                                <div class="col text-end">
-                                    <h6 class="mb-1">Order Delivered</h6>
-                                    <p class="mb-0 text-muted fs-12 fw-medium">20 Feb 2024, 01:00 PM</p>
-                                </div> <!-- end col -->
-                            </div> <!-- end row -->                                    
-                            <div class="bg-primary-subtle p-2 border-dashed border-primary rounded mt-3">
-                                <span class="text-primary fw-semibold">Note :</span><span class="text-primary fw-normal"> Ship all the ordered item together by monday and i send you an email please check. Thanks!</span>
                             </div>
                         </div><!--card-body-->
                     </div><!--end card-->
@@ -114,29 +75,29 @@
                                 @if ($order->shiptype == '1')
                                     <div class="d-flex justify-content-between">
                                         <p class="text-body fw-semibold">Tiền hàng :</p>
-                                        <p class="text-body-emphasis fw-semibold">{{$order->total_money-15000}}đ</p>
+                                        <p class="text-body-emphasis fw-semibold">{{ number_format($order->total_money-15000, 0, ',', '.') }}đ</p>
                                     </div>
                                     <div class="d-flex justify-content-between">
                                         <p class="text-body fw-semibold">Phí vận chuyển :</p>
-                                        <p class="text-danger fw-semibold">15000đ</p>
+                                        <p class="text-danger fw-semibold">15.000đ</p>
                                     </div>
                                     @else
                                     <div class="d-flex justify-content-between">
                                         <p class="text-body fw-semibold">Tiền hàng :</p>
-                                        <p class="text-body-emphasis fw-semibold">{{$order->total_money-30000}}đ</p>
+                                        <p class="text-body-emphasis fw-semibold">{{ number_format($order->total_money-30000, 0, ',', '.') }}đ</p>
                                     </div>
                                     <div class="d-flex justify-content-between">
                                         <p class="text-body fw-semibold">Phí vận chuyển :</p>
-                                        <p class="text-danger fw-semibold">30000đ</p>
+                                        <p class="text-danger fw-semibold">30.000đ</p>
                                     </div>
                                         @endif
                                         <div class="d-flex justify-content-between">
                                             <p class="text-body fw-semibold">Tổng thanh toán :</p>
-                                            <p class="text-body-emphasis fw-semibold">{{$order->total_money}}</p>
+                                            <p class="text-body-emphasis fw-semibold">{{ number_format($order->total_money, 0, ',', '.') }} đ</p>
                                         </div>
                                         <div class="d-flex justify-content-between">
                                             @if ($order->pay == '1')
-                                            <p class="text-body fw-semibold">Thanh toán khi nhận hàng :</p>
+                                            <p class="text-body fw-semibold">Thanh toán khi nhận hàng</p>
                                             @else
                                             <p class="text-body fw-semibold">Thanh toán vnPay</p>
                                             @endif
@@ -146,7 +107,8 @@
                             <hr class="hr-dashed">
                             <div class="d-flex justify-content-between">
                                 @if ($order->pay == '1')
-                                <h4 class="mb-0">Tổng thu  : {{$order->total_money}}đ</h4>
+                                <h4 class="mb-0">Tổng thu   : {{ number_format($order->total_money, 0, ',', '.') }} đ</h4>
+
                                         @else
                                 <h4 class="mb-0">Tổng thu  : 0đ</h4>
                                         @endif</p>
@@ -179,7 +141,7 @@
                                 </div>
                                 <div class="d-flex justify-content-between mb-2">
                                     <p class="text-body fw-semibold"><i class="iconoir-dollar-circle text-secondary fs-20 align-middle me-1"></i>Tổng thanh toán :</p>
-                                    <p class="text-body-emphasis fw-semibold"><span class="text-primary">{{$order->total_money}}</p>
+                                    <p class="text-body-emphasis fw-semibold"><span class="text-primary">{{ number_format($order->total_money, 0, ',', '.') }}đ</p>
                                 </div>
                                 <div class="d-flex justify-content-between mb-2">
                                     <p class="text-body fw-semibold"><i class="iconoir-calendar text-secondary fs-20 align-middle me-1"></i>Ngày đặt :</p>
