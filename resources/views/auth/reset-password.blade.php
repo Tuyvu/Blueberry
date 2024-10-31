d43800bb7a7ade8d9fa6ae45b2599e6aae7e846a
Tuyvu <130845326+Tuyvu@users.noreply.github.com>
2024-10-31 13:22:51 +0700

    Update reset-password.blade.php

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,6 +5,9 @@
             <div class="col-12">
                 <div class="section-title bb-center aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
                     <div class="section-detail">
+                        <a href="{{route('user.index')}}">
+                                        <img src="{{asset('assets')}}/img/logo/logo.png" alt="logo" style="margin-bottom: 26px;margin-right: 8px;" class="light">
+                        </a>
                         <h2 class="bb-title">Xác Nhận <span>Mật khẩu</span></h2>
                     </div>
                 </div>
